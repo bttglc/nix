369af4f4e4fbcabf3dc843ba10093acc35225cba
Gianluca Bottiglieri <bottiglierigianluca@icloud.com>
2025-07-03 22:37:01 +0200

    add brews, casks, mas

--- darwin/brew.nix
+++ darwin/brew.nix
@@ -3,11 +3,11 @@
     enable = true;
     
     brews = [
+      # CLI utility to get Apple Store ID
+      "mas"
+
       # Mesh net VPN based on Wireguard
       "tailscale"
-
-      # Text expansion utility
-      "espanso"
     ];
 
     casks = [
@@ -18,7 +18,10 @@
       "bitwarden"
 
       # Wine-based software to execute Windows programs
-      "crossover"
+      "crossover"  
+
+      # Text expansion utility
+      "espanso"
 
       # Powerful automation tool
       "hammerspoon"
@@ -35,6 +38,15 @@
       # Torrent client
       "transmission"
     ];
+
+    masApps = 
+      {
+        # Video player
+	      "ElMediaPlayer" = 1044549675;
+
+        # To-do app based on GTD philosophy
+        "Things" = 904280696;
+	    };
    
     onActivation = {
       autoUpdate = true;  # Update Homebrew automatically
