dc5671dfd1a86024cff89d7e7097a54c1071c224
Gianluca Bottiglieri <bottiglierigianluca@icloud.com>
2025-07-03 22:23:48 +0200

    add brews and casks

--- darwin/brew.nix
+++ darwin/brew.nix
@@ -2,6 +2,14 @@
   homebrew = {
     enable = true;
     
+    brews = [
+      # Mesh net VPN based on Wireguard
+      "tailscale"
+
+      # Text expansion utility
+      "espanso"
+    ];
+
     casks = [
       # Battery utility
       "aldente"
@@ -9,14 +17,23 @@
       # Password manager
       "bitwarden"
 
+      # Wine-based software to execute Windows programs
+      "crossover"
+
       # Powerful automation tool
       "hammerspoon"
 
       # Cube-based game focusing on mining and crafting
       "minecraft"
       
+      # Gaming client and store
+      "steam"
+
       # Window and tiling manager
       "rectangle"
+
+      # Torrent client
+      "transmission"
     ];
    
     onActivation = {
