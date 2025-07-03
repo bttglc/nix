6c60d154c03aaee3072e25835c84aa09811e9889
Gianluca Bottiglieri <bottiglierigianluca@icloud.com>
2025-07-03 20:05:56 +0200

    update persistent apps

--- darwin/persistent-apps.nix
+++ darwin/persistent-apps.nix
@@ -4,6 +4,9 @@
   system.defaults.dock.persistent-apps = [
     "${pkgs.alacritty}/Applications/Alacritty.app"
     "${pkgs.firefox}/Applications/Firefox.app"
+    "/Applications/Minecraft.app" 
+    "/Applications/Bitwarden.app"
+    "/Applications/Hammerspoon.app/"
     "/System/Applications/System\ Settings.app"
   ];
 }
