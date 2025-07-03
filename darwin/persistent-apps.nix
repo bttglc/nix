7fc60d6583c94ca92ab373dadc2f9adb56226682
Gianluca Bottiglieri <bottiglierigianluca@icloud.com>
2025-07-03 23:03:45 +0200

    set more persistent apps

--- darwin/persistent-apps.nix
+++ darwin/persistent-apps.nix
@@ -2,9 +2,12 @@
 
 {
   system.defaults.dock.persistent-apps = [
+    "/Applications/Things3.app"
     "${pkgs.alacritty}/Applications/Alacritty.app"
     "${pkgs.firefox}/Applications/Firefox.app"
+    "/Applications/Steam.app/"
     "/Applications/Minecraft.app" 
+    "/Applications/CrossOver.app/"
     "/Applications/Bitwarden.app"
     "/Applications/Hammerspoon.app/"
     "/System/Applications/System\ Settings.app"
