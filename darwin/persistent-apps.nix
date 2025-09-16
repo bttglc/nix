367a78d1593b7b6ff0f1f67383e7236f94d59c94
Gianluca Bottiglieri <bottiglierigianluca@icloud.com>
2025-09-16 11:10:43 +0200

    fix persisten app display of VSCodium

--- darwin/persistent-apps.nix
+++ darwin/persistent-apps.nix
@@ -7,6 +7,7 @@
     "/System/Applications/Reminders.app"
     "${pkgs.obsidian}/Applications/Obsidian.app"
     "${pkgs.alacritty}/Applications/Alacritty.app"
+    "${pkgs.vscode}/Applications/VSCodium.app"
     "/Applications/Claude.app"
     "${pkgs.firefox}/Applications/Firefox.app"
     "/System/Applications/Mail.app"
